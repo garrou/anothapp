802781cdf2257550d337621f730bafb719f32cd2
Adrien <39885692+1-irdA@users.noreply.github.com>
2023-01-19 23:43:59 +0100

    Make home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Account from "./views/Account";
 import Home from "./views/Home";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/account" element={<Account />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,59 +1,99 @@
-import { Avatar, Box, CssBaseline, Grid, Paper, Typography } from "@mui/material"
-import Button from "@mui/material/Button";
+import { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CssBaseline from '@mui/material/CssBaseline';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { Avatar, CardMedia, Stack } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
+    const server: string | undefined = process.env.REACT_APP_SERVER;
+    const navigate = useNavigate();
+    const [shows, setShows] = useState([]);
 
-    const handleSubmit = () => {
+    useEffect(() => {
+        (async () => {
+            const resp: Response = await fetch(`${server}/intro/images`);
+            const data: any = await resp.json();
+            setShows(data);
+        })();
+    }, []);
 
+    const onClick = async () => {
+        window.open(`${server}/auth/google`, '_self');
     }
 
     return (
-        <Grid container component="main" sx={{ height: '100vh' }}>
+        <>
             <CssBaseline />
-            <Grid
-                item
-                xs={false}
-                sm={4}
-                md={7}
-                sx={{
-                    backgroundImage: 'url(logo.png)',
-                    backgroundRepeat: 'no-repeat',
-                    backgroundColor: (t) =>
-                        t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                }}
-            />
-            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+            <main>
                 <Box
                     sx={{
-                        my: 5,
-                        mx: 4,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
+                        bgcolor: 'background.paper',
+                        pt: 8,
+                        pb: 6,
                     }}
                 >
-                    <Avatar 
-                        sx={{ m: 1, width: 250, height: 250, display: { sm: 'none' } }} 
-                        src="logo.png" />
-                    <Typography component="h1" variant="h5">
-                        A N O T H A P P
-                    </Typography>
-                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-                        <Button
-                            color="inherit"
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3 }}
-                            startIcon={<Avatar src={'google.png'} />}
+                    <Container maxWidth="sm">
+                        <Typography
+                            component="h1"
+                            variant="h2"
+                            align="center"
+                            color="text.primary"
+                            gutterBottom
                         >
-                            Se connecter avec Google
-                        </Button>
-                    </Box>
+                            Anothapp
+                        </Typography>
+                        <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                            Un site permettant de gérer vos séries, vos saisons et vos épisodes.
+                        </Typography>
+                        <Stack
+                            sx={{ pt: 4 }}
+                            direction="row"
+                            spacing={2}
+                            justifyContent="center"
+                        >
+                            <Button color="inherit" variant="contained" onClick={onClick} startIcon={<Avatar src={'img/google.png'} />}>
+                                Se connecter avec Google
+                            </Button>
+                        </Stack>
+                    </Container>
                 </Box>
-            </Grid>
-        </Grid>
-    )
+                <Container sx={{ py: 8 }} maxWidth="md">
+                    <Grid container spacing={4}>
+                        {shows.map(s => (
+                            <Grid item key={s['showId']} xs={12} sm={6} md={4}>
+                                <Card
+                                    sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                                >
+                                    <CardMedia
+                                        component="img"
+                                        sx={{
+                                            pt: '56.25%',
+                                        }}
+                                        image={s['poster']}
+                                        alt="Poster"
+                                    />
+                                    <CardContent sx={{ flexGrow: 1 }}>
+                                        <Typography gutterBottom variant="h5" component="h2" align="center">
+                                            {s['title']}
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Container>
+            </main>
+
+            <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+                <Button color="inherit" variant="outlined" href="https://github.com/garrou/anothapp" startIcon={<GitHubIcon />}>Github</Button>
+            </Box>
+        </>
+    );
 };
